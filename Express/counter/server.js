a83cafe9e17b5a730389a302bedf3a3a28958149
Abdulaziz Saud <azoz-saud@live.com>
2019-10-04 13:01:34 +0300

    finished Express counter

---
+++ Express/counter/server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+const session = require('express-session');
+
+app.use(express.static(__dirname + "/static"));
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+
+app.use(session({
+    secret: 'keyboardkitteh',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 }
+}))
+
+
+app.get('/', (req, res) => {
+
+    if (req.session.count) {
+        req.session.count += 1;
+    }
+    else {
+        req.session.count = 1;
+
+    }
+    res.locals.count = req.session.count;
+    // var count = res.session.count;
+    // console.log(res.session.count) ;
+
+    res.render('index');
+});
+
+app.post('/add_two', (req, res) => {
+    req.session.count += 1;
+    res.redirect('/');
+});
+
+app.post('/reset', (req, res) => {
+    req.session.count = 0;
+    res.redirect('/');
+});
+
+app.listen(8000, () => console.log("listening on port 8000"));
