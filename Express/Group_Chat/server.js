13bdd48be88862c17268aa0d4ee7181c9ba17ac0
Abdulaziz Saud <azoz-saud@live.com>
2019-10-08 03:26:17 +0300

    finished chat group

--- Express/Group_Chat/server.js
+++ Express/Group_Chat/server.js
@@ -18,14 +18,28 @@ io.on('connection', function (socket) {
 
     //Disconnect
     socket.on('disconnect', function (data) {
+        users.splice(users.indexOf(socket.username), 1);
+        updateUserNames();
         connections.splice(connections.indexOf(socket), 1);
         console.log("Disconnected, %s socketsconnectd", connections.length);
     });
     //Send Message
-    socket.on('send message', function(data){
+    socket.on('send message', function (data) {
         console.log(data);
-        io.sockets.emit('new message', {msg: data});
+        io.sockets.emit('new message', { msg: data, user: socket.username});
     });
+
+    //New user
+    socket.on('new user', function (data, callback) {
+        callback(true);
+        socket.username = data;
+        users.push(socket.username);
+        updateUserNames();
+    });
+
+    function updateUserNames() {
+        io.sockets.emit('get users', users);
+    }
 });
 http.listen(3000, function () {
     console.log('listening on *:3000');
