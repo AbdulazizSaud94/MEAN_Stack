ef0c1af57ec3aa4aaf8f1b9feea843984f5bbfc2
Abdulaziz Saud <azoz-saud@live.com>
2019-10-15 05:00:46 +0300

    Message Board 90% done

--- MongoDB/Message_Board/models/message.js
+++ MongoDB/Message_Board/models/message.js
@@ -1,15 +1,16 @@
 let mongoose = require('mongoose');
 
+const CommentSchema = new mongoose.Schema({
+    commenter: { type: String, required: [true, "Comments must have a sender name"], minlength: [3, "Commenter name must be of length 3 or more"]},
+    comment: { type: String, required: [true, "Comments must have content"], minlength: [3, "Comment must be of length 3 or more"] },
+}, { timestamps: true })
 
 const MessageSchema = new mongoose.Schema({
-    name: { type: String, required: [true, "Messages must have a sender name"] },
-    content: { type: String, required: [true, "Messages must have content"] },
+    name: { type: String, required: [true, "Messages must have a sender name"], minlength: [3, "Name must be of length 3 or more"] },
+    content: { type: String, required: [true, "Messages must have content"], minlength: [3, "Message must be of length 3 or more"] },
     comments: [CommentSchema],
 }, { timestamps: true });
 
-const CommentSchema = new mongoose.Schema({
-    commenter: {type: String, required: [true, "Comments must have a sender name"]},
-    comment: {type: String, required: [true, "Comments must have content"]},
-  }, {timestamps: true})
 
+let Comment = module.exports = mongoose.model('Comment', CommentSchema);
 let Message = module.exports = mongoose.model('Message', MessageSchema);
--- MongoDB/Message_Board/server.js
+++ MongoDB/Message_Board/server.js
@@ -34,30 +34,102 @@ db.on('error', function (err) {
     console.log(err);
 });
 
-let Quote = require('./models/message');
+const CommentSchema = new mongoose.Schema({
+    commenter: { type: String, required: [true, "Comments must have a sender name"], minlength: [3, "Commenter name must be of length 3 or more"] },
+    comment: { type: String, required: [true, "Comments must have content"], minlength: [3, "Comment must be of length 3 or more"] },
+}, { timestamps: true })
+
+const MessageSchema = new mongoose.Schema({
+    name: { type: String, required: [true, "Messages must have a sender name"], minlength: [3, "Name must be of length 3 or more"] },
+    content: { type: String, required: [true, "Messages must have content"], minlength: [3, "Message must be of length 3 or more"] },
+    comments: [CommentSchema],
+}, { timestamps: true });
+
+mongoose.model('Message', MessageSchema);
+var Message = mongoose.model('Message');
+mongoose.model('Comment', CommentSchema);
+var Comment = mongoose.model('Comment');
+
 
 app.get('/', (req, res) => {
-    res.render('index');
+    Message.find({}).sort({ createdAt: 'desc' }).find(function (err, posts) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.render('index', {
+                posts: posts,
+            });
+        }
+    });
 });
 
-app.post('add_message', (req, res) => {
-    req.session.quoteErr = "";
-    req.session.nameErr = "";
-    console.log(req.body);
-    if (req.body.name.length < 3) {
-        req.session.nameErr = "Name is too short";
-        console.log(req.session.nameErr);
-        res.redirect("/");
-    }
-    if (req.body.quote.length < 5) {
-        req.session.quoteErr = "Quote is too short";
-        console.log(req.session.quoteErr);
-        res.redirect("/");
+app.post('/add_message', (req, res) => {
+    console.log("Message DATA", req.body);
+    var message = new Message({ name: req.body.message_name, content: req.body.message_text });
+    message.save(function (err) {
+        if (err) {
+            console.log("We have an error!", err);
+            // adjust the code below as needed to create a flash message with the tag and content you would like
+            for (var key in err.errors) {
+                req.flash('registration', err.errors[key].message);
+            }
+            res.redirect('/');
+        }
+        else {
+            console.log('successfully added a Message!');
+            res.redirect('/');
+        }
+    })
+});
 
-    }
-    const quote = new Quote(req.body);
-    quote.save()
-        .then(() => res.redirect('/quotes'));
+// app.post('/add_comment/:id', (req, res) => {
+//     console.log("Comment DATA", req.body);
+//     console.log(`msg id:${req.params.id}`);
+//     Comment.create(req.body, function (err, data) {
+//         if (err) {
+//             // handle the error from creating comment
+//             console.log("error from creating comment");
+//             res.redirect('/');
+//         }
+//         else {
+//             Message.findOneAndUpdate({ _id: req.params.id }, { $push: { comments: data } }, function (err, data) {
+//                 if (err) {
+//                     // handle the error from trying to update the user
+//                     console.log("error from trying to update the message");
+//                     res.redirect('/');
+//                 }
+//                 else {
+//                     console.log("Comment added");
+//                     res.redirect('/');
+//                 }
+//             })
+//         }
+//     });
+// });
+
+app.post('/add_comment/:id', function (req, res) {
+    Message.findOne({ _id: req.params.id }, function (err, message) {
+        var comment = new Comment(
+            {
+                commenter: req.body.comment_name,
+                comment: req.body.comment_text,
+            });
+        console.log(comment);
+
+        message.comments.push(comment);
+        message.save(function (err) {
+            if (err) {
+                console.log('something went wrong');
+                console.log(comment.errors);
+                res.redirect('/')
+            }
+            else {
+                console.log('successfully added a Comment!');
+                res.redirect('/');
+            }
+        });
+    });
 });
 
 app.listen(8000, () => console.log("listening on port 8000"));
